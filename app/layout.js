d1397e9e44480e16d960032dcd6c2532fad71d5d
Carlos Betancourt <carlos.betancourt@jalisco.gob.mx>
2022-11-22 09:40:35 -0600

    Loading component and pages

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
-
 import { Navigation } from '../components/Navigation';
+import { font } from './font';
 
 export default function RootLayout({ children }) {
   return (
@@ -8,7 +8,8 @@ export default function RootLayout({ children }) {
       <head>
         <title>My first app with Next 13</title>
       </head>
-      <body>
+      {/* <body className={font.className}> */}
+      <body className={font.variable}>
         <Navigation />
         {children}
       </body>
